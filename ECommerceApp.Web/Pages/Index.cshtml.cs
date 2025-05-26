0b46d19ef945e841ea18a49e0c6aa504f5b2cbef
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-26 08:33:53 +0600

    this will be later

--- ECommerceApp.Web/Pages/Index.cshtml.cs
+++ ECommerceApp.Web/Pages/Index.cshtml.cs
@@ -1,19 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
+using ECommerceApp.Web.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ECommerceApp.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly HttpClient _http;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(IHttpClientFactory httpClientFactory)
     {
-        _logger = logger;
+        _http = httpClientFactory.CreateClient("API");
     }
 
-    public void OnGet()
-    {
+    public List<Product> Products { get; set; } = new();
 
+    public async Task OnGetAsync()
+    {
+        var result = await _http.GetFromJsonAsync<ProductResponse>("api/products?page=1&pageSize=8");
+        Products = result?.Products ?? new List<Product>();
+    }
+
+    public class ProductResponse
+    {
+        public int Total { get; set; }
+        public List<Product> Products { get; set; } = new();
     }
 }
---
+++ ECommerceApp.Web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./Pages/**/*.{cshtml,html,js}", "./wwwroot/**/*.{cshtml,html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
