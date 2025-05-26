fe5d56b59c9a545fcbd9cae5668bc835107651c1
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-05-26 07:45:40 +0600

    update program files

--- ECommerceApp.API/Program.cs
+++ ECommerceApp.API/Program.cs
@@ -2,13 +2,17 @@ using ECommerceApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+builder.Services.AddAuthorization();
 
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,5 +21,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
 app.Run();
